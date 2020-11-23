1e3d7743d18b696556199f3d77a0f3704b1bae46
chaychan <chencanwei@jmfen.cn>
2020-11-23 16:56:00 +0800

    整理演示的代码

--- demo/src/main/java/com/chaychan/bottombarlayout/BaseViewPagerActivity.java
+++ demo/src/main/java/com/chaychan/bottombarlayout/BaseViewPagerActivity.java
@@ -1,15 +1,9 @@
 package com.chaychan.bottombarlayout;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
 
-import com.chaychan.library.BottomBarItem;
 import com.chaychan.library.BottomBarLayout;
 
 import java.util.ArrayList;
@@ -24,11 +18,9 @@ import androidx.viewpager.widget.ViewPager;
 public abstract class BaseViewPagerActivity extends AppCompatActivity {
 
     private ViewPager mVpContent;
-    private BottomBarLayout mBottomBarLayout;
+    protected BottomBarLayout mBottomBarLayout;
 
     private List<TabFragment> mFragmentList = new ArrayList<>();
-    private RotateAnimation mRotateAnimation;
-    private Handler mHandler = new Handler();
 
     protected abstract String[] getFragmentContents();
 
@@ -59,71 +51,14 @@ public abstract class BaseViewPagerActivity extends AppCompatActivity {
         }
     }
 
-    private void initListener() {
+    public void initListener() {
         mVpContent.setAdapter(new MyAdapter(getSupportFragmentManager()));
         mBottomBarLayout.setViewPager(mVpContent);
-        mBottomBarLayout.setOnItemSelectedListener(new BottomBarLayout.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(final BottomBarItem bottomBarItem, int previousPosition, final int currentPosition) {
-                Log.i("MainActivity", "position: " + currentPosition);
-                if (currentPosition == 0) {
-                    //如果是第一个，即首页
-                    if (previousPosition == currentPosition) {
-                        //如果是在原来位置上点击,更换首页图标并播放旋转动画
-                        if (mRotateAnimation != null && !mRotateAnimation.hasEnded()){
-                            //如果当前动画正在执行
-                            return;
-                        }
-
-                        bottomBarItem.setSelectedIcon(R.mipmap.tab_loading);//更换成加载图标
-
-                        //播放旋转动画
-                        if (mRotateAnimation == null) {
-                            mRotateAnimation = new RotateAnimation(0, 360,
-                                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
-                                    0.5f);
-                            mRotateAnimation.setDuration(800);
-                            mRotateAnimation.setRepeatCount(-1);
-                        }
-                        ImageView bottomImageView = bottomBarItem.getImageView();
-                        bottomImageView.setAnimation(mRotateAnimation);
-                        bottomImageView.startAnimation(mRotateAnimation);//播放旋转动画
-
-                        //模拟数据刷新完毕
-                        mHandler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                boolean tabNotChanged = mBottomBarLayout.getCurrentItem() == currentPosition; //是否还停留在当前页签
-                                bottomBarItem.setSelectedIcon(R.mipmap.tab_home_selected);//更换成首页原来选中图标
-                                cancelTabLoading(bottomBarItem);
-                            }
-                        }, 3000);
-                        return;
-                    }
-                }
-
-                //如果点击了其他条目
-                BottomBarItem bottomItem = mBottomBarLayout.getBottomItem(0);
-                bottomItem.setSelectedIcon(R.mipmap.tab_home_selected);//更换为原来的图标
-
-                cancelTabLoading(bottomItem);//停止旋转动画
-            }
-        });
 
         mBottomBarLayout.setUnread(0, 20);//设置第一个页签的未读数为20
         mBottomBarLayout.setUnread(1, 1001);//设置第二个页签的未读数
         mBottomBarLayout.showNotify(2);//设置第三个页签显示提示的小红点
         mBottomBarLayout.setMsg(3, "NEW");//设置第四个页签显示NEW提示文字
-    }
-
-    /**
-     * 停止首页页签的旋转动画
-     */
-    private void cancelTabLoading(BottomBarItem bottomItem) {
-        Animation animation = bottomItem.getImageView().getAnimation();
-        if (animation != null) {
-            animation.cancel();
-        }
     }
 
     class MyAdapter extends FragmentStatePagerAdapter {
--- demo/src/main/java/com/chaychan/bottombarlayout/ViewPagerDemoActivity.java
+++ demo/src/main/java/com/chaychan/bottombarlayout/ViewPagerDemoActivity.java
@@ -1,11 +1,23 @@
 package com.chaychan.bottombarlayout;
 
+import android.os.Handler;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import com.chaychan.library.BottomBarItem;
+import com.chaychan.library.BottomBarLayout;
+
 /**
  * @author ChayChan
  * @description: viewPager demo
  * @date 2020/11/21  15:18
  */
 public class ViewPagerDemoActivity extends BaseViewPagerActivity{
+
+    private RotateAnimation mRotateAnimation;
+    private Handler mHandler = new Handler();
 
     @Override
     protected String[] getFragmentContents() {
@@ -21,5 +33,67 @@ public class ViewPagerDemoActivity extends BaseViewPagerActivity{
     public void initView() {
         super.initView();
         getSupportActionBar().setTitle(ViewPagerDemoActivity.class.getSimpleName());
+    }
+
+    @Override
+    public void initListener() {
+        super.initListener();
+        mBottomBarLayout.setOnItemSelectedListener(new BottomBarLayout.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(final BottomBarItem bottomBarItem, int previousPosition, final int currentPosition) {
+                Log.i("MainActivity", "position: " + currentPosition);
+                if (currentPosition == 0) {
+                    //如果是第一个，即首页
+                    if (previousPosition == currentPosition) {
+                        //如果是在原来位置上点击,更换首页图标并播放旋转动画
+                        if (mRotateAnimation != null && !mRotateAnimation.hasEnded()){
+                            //如果当前动画正在执行
+                            return;
+                        }
+
+                        bottomBarItem.setSelectedIcon(R.mipmap.tab_loading);//更换成加载图标
+
+                        //播放旋转动画
+                        if (mRotateAnimation == null) {
+                            mRotateAnimation = new RotateAnimation(0, 360,
+                                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                                    0.5f);
+                            mRotateAnimation.setDuration(800);
+                            mRotateAnimation.setRepeatCount(-1);
+                        }
+                        ImageView bottomImageView = bottomBarItem.getImageView();
+                        bottomImageView.setAnimation(mRotateAnimation);
+                        bottomImageView.startAnimation(mRotateAnimation);//播放旋转动画
+
+                        //模拟数据刷新完毕
+                        mHandler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                boolean tabNotChanged = mBottomBarLayout.getCurrentItem() == currentPosition; //是否还停留在当前页签
+                                bottomBarItem.setSelectedIcon(R.mipmap.tab_home_selected);//更换成首页原来选中图标
+                                cancelTabLoading(bottomBarItem);
+                            }
+                        }, 3000);
+                        return;
+                    }
+                }
+
+                //如果点击了其他条目
+                BottomBarItem bottomItem = mBottomBarLayout.getBottomItem(0);
+                bottomItem.setSelectedIcon(R.mipmap.tab_home_selected);//更换为原来的图标
+
+                cancelTabLoading(bottomItem);//停止旋转动画
+            }
+        });
+    }
+
+    /**
+     * 停止首页页签的旋转动画
+     */
+    private void cancelTabLoading(BottomBarItem bottomItem) {
+        Animation animation = bottomItem.getImageView().getAnimation();
+        if (animation != null) {
+            animation.cancel();
+        }
     }
 }
